f9846a2d8d4758868e923293f841ccc216ce7209
crashbit <crashbit@gmail.com>
2023-04-22 16:54:10 +0200

    Revert

--- install.sh
+++ install.sh
@@ -30,9 +30,7 @@ sudo python3 -m venv /opt/pvpccheap/venv
 # Activate virtual environment
 sudo -H -u pvpccheap bash -c "source /opt/pvpccheap/venv/bin/activate"
 # Install package
-sudo -H -u pvpccheap bash -c "pip install dist/pvpccheap-0.1-py3-none-any.whl"
-# Deactivate virtual environment
-sudo -H -u pvpccheap bash -c "deactivate"
+pip install dist/pvpccheap-0.1-py3-none-any.whl
 
 # Assign permissions to the application folder
 APP_PATH="/opt/pvpccheap"
