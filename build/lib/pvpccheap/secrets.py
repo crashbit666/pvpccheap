5c91c8c0cd73ee4062ef1292a7dab7fe5eaf56de
crashbit <crashbit@gmail.com>
2023-04-23 16:02:18 +0200

    Change a secret file to serve correct json file folder

--- build/lib/pvpccheap/secrets.py
+++ build/lib/pvpccheap/secrets.py
@@ -4,6 +4,6 @@ secrets = {
     'TOKEN': "84df043582208624ed92672a280bc9862df5b00220b2645f53edcd0d58c3015d",
     'URL': "https://api.esios.ree.es/indicators/1001",
     'WEBHOOKS_KEY': 'cPC_vSVqJfUfu9SL0MwjZw',
-    'JSON_FILE': '/home/crashbit/Programació/Projectes/pvpccheap/pvpccheap-firebase-adminsdk-yel11-65a454a38e.json',
+    'JSON_FILE': 'pvpccheap-firebase-adminsdk-yel11-65a454a38e.json',
     'FIREBASE_URL': 'https://pvpccheap-default-rtdb.europe-west1.firebasedatabase.app/'
 }
